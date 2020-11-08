884ec1c3b4677bcb6dcc0036ccc27a2d0916fba2
yong <Yong_GJ@163.com>
2020-11-08 11:37:41 +0800

    [CHORE]构建python3编程 从入门到实践项目01
    Signed-off-by: yong <Yong_GJ@163.com>

---
+++ com/chapter_01/section_01/task_01_helloWorld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/8 11:12
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_01_helloWorld.py
+# @Software: PyCharm
+
+
+print("hello python world！")
