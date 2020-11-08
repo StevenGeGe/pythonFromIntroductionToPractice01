8bfd08e7bad4430860074d8acae75089831f3862
yong <Yong_GJ@163.com>
2020-11-08 17:20:34 +0800

    [ADD]python3中 访问列表的索引
    Signed-off-by: yong <Yong_GJ@163.com>

---
+++ com/chapter_03/section_01/task_3.1.3_listUseValue.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/8 17:16
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_3.1.3_listUseValue.py
+# @Software: PyCharm
+
+# 使用列表中的值
+# 可以像使用其他变量一样使用列表中的各个值。
+
+bicycles_6 = ['trek', 'cannondale', 'redline', 'specialized']
+meseage = "My first bicycle was a " + bicycles_6[0].title() + "."
+print(meseage)  # 输出: My first bicycle was a Trek.
