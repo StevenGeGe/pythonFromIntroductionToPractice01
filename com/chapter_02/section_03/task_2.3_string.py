41b2a84e55dc329cdb9e2a9e09aed064e5dccbe4
yong <Yong_GJ@163.com>
2020-11-08 14:56:15 +0800

    [ADD][title]python3中字符串的大小写转换
    Signed-off-by: yong <Yong_GJ@163.com>

---
+++ com/chapter_02/section_03/task_2.3_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/8 14:44
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_2.3_string.py
+# @Software: PyCharm
+
+
+# title() : 以首字母大写的方式显示每个单词，即将每个单词的首字母全部大写或者全部小写。
+
+# 更改字符串的小写
+name_big = "ada love"
+print(name_big.title())  # 输出: Ada Love
+
+# 更改字符串的大写
+name_small = "All The World"
+print(name_small.title())  # 输出: All The World
