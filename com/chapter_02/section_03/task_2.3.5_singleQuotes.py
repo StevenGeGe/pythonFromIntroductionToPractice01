f09e0051a31d20b4de1ca1ee4a321ca243903735
yong <Yong_GJ@163.com>
2020-11-08 16:11:31 +0800

    [ADD]python3中用单引号和双引号的使用区别
    Signed-off-by: yong <Yong_GJ@163.com>

---
+++ com/chapter_02/section_03/task_2.3.5_singleQuotes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/8 16:06
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_2.3.5_singleQuotes.py
+# @Software: PyCharm
+
+# 单引号的使用
+# python3中，可以使用单引号作为字符串，也可以使用双引号作为字符串，但要注意使用和区分。
+
+# 单引号
+str_quotes = 'nihao,quotes'
+str_quotation = "nihao,quotation"
+
+# 错误 实例
+# str_error = 'nihao，python's
+
+print(str_quotes, str_quotation)
