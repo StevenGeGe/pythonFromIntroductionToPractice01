87cea82106f6ca12b6d8575a8d2f330f136ce038
yong <Yong_GJ@163.com>
2020-11-08 14:46:36 +0800

    [ADD]python3中变量的定义
    Signed-off-by: yong <Yong_GJ@163.com>

---
+++ com/chapter_02/section_02/task_2.2_variable.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/8 12:01
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_2.2_variable.py
+# @Software: PyCharm
+
+
+'''
+2.2.1 变量的命名和使用
+
+
+
+'''
+
+message = "hello world 1"
+print(message)
+
+message = "hello world 2"
+print(message)
+
+# 变量名写错
+# print(messae)
