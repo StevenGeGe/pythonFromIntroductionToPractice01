fa1587577f57c465f3840e4e2f9d8ee078b978b2
yong <Yong_GJ@163.com>
2020-11-08 15:41:02 +0800

    [ADD]python3中用制表符或者换行符代替空白
    Signed-off-by: yong <Yong_GJ@163.com>

---
+++ com/chapter_02/section_03/task_2.3.3_newline.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/8 15:32
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_2.3.3_newline.py
+# @Software: PyCharm
+
+# 换行符
+# 用换行符或者制表符代替空白
+# \t : 制表符，一般四个空格
+# \n : 换行符
+
+# 正常
+print("正常:nihao python")
+# 加了制表符
+print("加了制表符:\tnihao python")
+
+# 正常
+print("正常:nihao python, nihao yong")
+# 加了换行符
+print("加了换行符:\n nihao\n python,\n nihao\n yong\n")
+
+'''
+输出:
+正常:nihao python
+加了制表符:	nihao python
+正常:nihao python, nihao yong
+加了换行符:
+ nihao
+ python,
+ nihao
+ yong
+
+'''
