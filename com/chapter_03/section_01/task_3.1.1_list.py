02f84cb72dbd1f2381626031dd2ee0f7ef06ad33
yong <Yong_GJ@163.com>
2020-11-08 16:50:59 +0800

    [ADD]python3中 列表的写法与访问
    Signed-off-by: yong <Yong_GJ@163.com>

---
+++ com/chapter_03/section_01/task_3.1.1_list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/8 16:37
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_3.1.1_list.py
+# @Software: PyCharm
+
+# 列表
+# 列表是由一系列特定顺序排列的元素组成。
+# 可以将任何东西加入到列表中，其中的元素可以没有任何关系，无论是字母，数字，家庭成员姓名。
+# 通常用（[]）来表示列表， 并用逗号分隔其中的元素。
+
+# 访问列表
+# 列表是有序集合，若访问列表中的任何元素，只需将该元素的位置或者索引告诉python即可。
+# 要访问列表元素，可指出列表的名称，再指出元素的索引，并将其放在括号内。
+
+# 列表的写法
+bicycles_1 = ['a', 'b', 'c', 'd']
+print(bicycles_1)  # 输出: ['a', 'b', 'c', 'd']
+
+bicycles_2 = ["aa", "bb", "cc", "dd"]
+print(bicycles_2)  # 输出:  ['aa', 'bb', 'cc', 'dd']
+
+bicycles_3 = ["aa" "bb" "cc""dd"]
+print(bicycles_3)  # 输出:  ['aabbccdd']
+
+# 访问列表元素
+bicycles_4 = ['a', 'b', 'c', 'd']
+print(bicycles_4[0])  # 输出: a
+
+bicycles_4 = ['aa', 'bb', 'cc', 'dd']
+print(bicycles_4[0].title())  # 输出: Aa
