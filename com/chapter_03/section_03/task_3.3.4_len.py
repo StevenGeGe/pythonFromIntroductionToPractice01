097c73d154424a7216bf531f4dbfad8f539ab276
StevenGeGe <Yong_GJ@163.com>
2020-11-09 19:51:49 +0800

    [ADD] 在python3中，len计算列表的大小

---
+++ com/chapter_03/section_03/task_3.3.4_len.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/9 19:44
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_3.3.4_len.py
+# @Software: PyCharm
+
+# 列表中的大小
+# len(list): 快速获取列表的长度。
+# 默认从1开始，而不是从0 开始。
+
+# 去掉print()后面的换行符
+# print(str, end="")
+
+carList = ['bmw', 'audi', 'toyota', 'subaru']
+print("carList大小：", end="")
+print(len(carList))  # 输出: ['bmw', 'audi', 'toyota', 'subaru']
