d4228fc840a6ea1facf4db98221ca5102d375c0d
StevenGeGe <Yong_GJ@163.com>
2020-11-10 21:12:00 +0800

    [ADD] 在python3中，使用for循环打印列表中内容

---
+++ com/chapter_04/section_01/task_4.1.1_for.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/10 20:58
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_4.1.1_for.py
+# @Software: PyCharm
+
+# for循环打印列表元素
+# 获取列表中的第一个元素(alice)，然后存储到变量中(magician)
+# 对于用于存储列表中每个元素的临时变量， 可以指定任何名称
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+cats = ['xiaoju', 'jiafei']
+dogs = ['erha', 'jinmao']
+
+for cat in cats:
+    print(cat)
+for dog in dogs:
+    print(dog)
