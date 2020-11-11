22379dce1f1acec2b6fbb9c63cb6f0cdd69adf1c
StevenGeGe <Yong_GJ@163.com>
2020-11-11 11:52:03 +0800

    [ADD] 在python3中，使用ranges输出范围数值

---
+++ com/chapter_04/section_03/task_4.3.1_range.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/11 11:47
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_4.3.1_range.py
+# @Software: PyCharm
+
+# 数值列表
+
+# 生成一系列的数字
+# range():
+# 如果使用ranges(),输出结果不符合预期，可以尝试将指定值加1或减1
+
+for values in range(1, 5):
+    print(values, end="")
+# 输出： 1 2 3 4
