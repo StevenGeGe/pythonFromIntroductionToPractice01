e2c032acbef1bd40273c426612a23dd96138b7bd
StevenGeGe <Yong_GJ@163.com>
2020-11-11 11:46:20 +0800

    [ADD] 在python3中，关于缩进的注意事项

---
+++ com/chapter_04/section_02/task_4.2.1_indentation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/11 11:41
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_4.2.1_indentation.py
+# @Software: PyCharm
+
+# 缩进
+# 如果没有缩进，或者错误缩进，会造成逻辑错误。
+# 循环后，不要错误缩进，不然会造成多次执行的错误。
+# 循环，不要遗忘冒号，不然解释器会报错。
