8a99d4660f5d342c6155697db6136d57ffbfd229
StevenGeGe <Yong_GJ@163.com>
2020-11-12 19:59:48 +0800

    [ADD] 在python3中，遍历切片

---
+++ com/chapter_04/section_04/task_4.4.2_forSlice.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/12 19:53
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_4.4.2_forSlice.py
+# @Software: PyCharm
+
+# 遍历切片
+
+players = ['a', 'b', 'c', 'd', 'e']
+print("here are first team:", end="")
+for player in players[2:4]:
+    print(player + " ", end="")
