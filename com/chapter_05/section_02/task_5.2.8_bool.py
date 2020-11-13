dc4dac393734368fa99fec2786a53ff2fb4f2199
StevenGeGe <Yong_GJ@163.com>
2020-11-13 20:58:24 +0800

    [ADD] 在python3中，bool布尔表达式的用法

---
+++ com/chapter_05/section_02/task_5.2.8_bool.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/13 20:52
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_5.2.8_bool.py
+# @Software: PyCharm
+
+# 布尔表达式
+# 要么为true， 要么为false
+
+game_active = True
+can_edit = False
+
+car = 'subaru'
+print("is cat == 'subaru'? i predict True")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? i predict False")
+print(car == 'audi')
