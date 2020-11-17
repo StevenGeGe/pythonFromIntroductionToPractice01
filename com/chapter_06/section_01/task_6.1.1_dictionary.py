9da28b4f6a58d50c92f2649e917ac2e50df824b1
StevenGeGe <Yong_GJ@163.com>
2020-11-17 20:03:32 +0800

    [ADD] 在python3中，字典(dictionary)的定义

---
+++ com/chapter_06/section_01/task_6.1.1_dictionary.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/17 18:52
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_6.1.1_dictionary.py
+# @Software: PyCharm
+
+# 字典
+# 字典存储的信息几乎不受限制
+# dictionary{}: 代表字典
+#           dictionary{'key_1':'value_1', 'key_2':'value_2'}
+# 获取字典内容：dictionary{'key'}
+
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
