0330a42fbec52fb8a6aa40d3882c20fc7c8307ba
StevenGeGe <Yong_GJ@163.com>
2020-11-19 20:52:01 +0800

    [ADD] 在python2中, 输入运算，raw_input()

---
+++ com/chapter_07/section_01/task_7.1.4_inputInPython2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/19 20:41
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_7.1.4_inputInPython2.py
+# @Software: PyCharm
+
+# 在python2.7 中的输入
+# raw_input(): 来进行输入操作， 也会解读为字符串
