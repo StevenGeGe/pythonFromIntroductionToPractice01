60295b0ad3617f061ff3b7f24dda922b6b8dc91a
StevenGeGe <Yong_GJ@163.com>
2020-11-19 20:52:14 +0800

    [ADD] 在python3中, while循环

---
+++ com/chapter_07/section_02/task_7.2.1_while.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/19 20:44
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_7.2.1_while.py
+# @Software: PyCharm
+
+# python3中的while循环
+# for 循环用于针对集合中的每个元素都一个代码块，
+# 而while 循环不断地运行，直到指定的条件不满足为止。
+
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
