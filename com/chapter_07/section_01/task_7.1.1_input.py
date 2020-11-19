7be54e414792531bd5a49e9415686f071bdd2d2c
StevenGeGe <Yong_GJ@163.com>
2020-11-19 20:09:19 +0800

    [ADD] 在python3中，了解输入函数input()

---
+++ com/chapter_07/section_01/task_7.1.1_input.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/19 20:03
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_7.1.1_input.py
+# @Software: PyCharm
+
+# python3中的输入
+
+# 函数input()：让程序暂停运行，等待用户输入一些文本。
+# 获取用户输入后，Python将其存储在一个变量中，以方便你使用。
+# input("你想输出的提示信息"), 并把输入的信息返回给其他变量。
+
+message = input("输入你想输入的信息：")
+print("你输入的信息是：" + message)
