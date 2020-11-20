2b2273eb1a868b6fc901b486c0ed4f570bb04703
StevenGeGe <Yong_GJ@163.com>
2020-11-20 21:31:25 +0800

    [ADD] 在python3中, 函数和while，if连用

---
+++ com/chapter_08/section_03/task_8.3.4_functionWhile.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/20 21:26
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_8.3.4_functionWhile.py
+# @Software: PyCharm
+
+
+def get_formatted_name(first_name, last_name):
+    """返回整洁的姓名"""
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+
+# 这是一个无限循环!
+# while True:
+#     print("\nPlease tell me your name:")
+#     f_name = input("First name: ")
+#     l_name = input("Last name: ")
+#     formatted_name = get_formatted_name(f_name, l_name)
+# print("\nHello, " + formatted_name + "!")
+
+
+def get_formatted_name(first_name, last_name):
+    """返回整洁的姓名"""
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+formatted_name = get_formatted_name(f_name, l_name)
+print("\nHello, " + formatted_name + "!")
