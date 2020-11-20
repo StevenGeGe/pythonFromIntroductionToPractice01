6f9c375c64a42df8071bc45ce32ccbb229500592
StevenGeGe <Yong_GJ@163.com>
2020-11-20 20:10:43 +0800

    [ADD] 在python3中, 定义函数

---
+++ com/chapter_08/section_01/task_8.1.1_function.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/20 20:02
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_8.1.1_function.py
+# @Software: PyCharm
+
+# 在python3中的函数
+# 用def Function_name()： 定义一个函数。
+
+'''输出美句'''
+
+
+def greet_user():
+    # 显示简单的问候
+    print("一个人知道自己为什么而活，就可以忍受任何一种生活。")
+
+
+# 调用函数
+greet_user()
