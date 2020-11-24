022e1be75590438344a99d3cec469b7540df8d9a
StevenGeGe <Yong_GJ@163.com>
2020-11-24 17:36:23 +0800

    [ADD] 在python3中, 函数编写指南

---
+++ com/chapter_08/section_07/task_8.7.1_functionGuide.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/24 17:17
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_8.7.1_functionGuide.py
+# @Software: PyCharm
+
+# python3 函数编写指南
+# 1、给函数指定描述性名称, 且只在其中使用小写字母。
+# 2、给模块命名时也要遵循上述约定。
+# 3、每个函数都应包含简要的阐述功能的注释，注释应该紧跟在函数后面，并采用文档字符串格式。
+# 4、给形参指定默认值时，等号两边不要有空格。
+# 5、对于函数调用中的关键字实参， 也要遵循此约束。
+# 6、如果形参很多，导致函数定义的长度超过了 79字符，
+#   可在函数定义中输入左括号后按回车键，并在下一行按两次Tab键，
+#   从而将形参列表和只缩进一层的函数体区分开来。
+
+
+# 6 、空两个tab
+def function_name(
+        parameter_0,
+        parameter_1,
+        parameter_2,
+        parameter_3,
+        parameter_4,
+        parameter_5):
+    function
+    body
