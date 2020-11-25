04a14c85f3a13a9102a114eb202128b43201fb9c
StevenGeGe <Yong_GJ@163.com>
2020-11-25 20:36:30 +0800

    [ADD] 在python3中, 导入模块中所有的类

---
+++ com/chapter_09/section_04/task_9.4.5_importAllClass.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/25 20:34
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_9.4.5_importAllClass.py
+# @Software: PyCharm
+
+# 导入模块中所有的类
+# from module_name import *
+
+# 不推荐这样使用。
+# 推荐使用：module_name.class_name 语法来访问类
