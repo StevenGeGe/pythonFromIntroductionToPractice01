f55ac61185a69a1f24831671fc0c12dc9ca81fc7
StevenGeGe <Yong_GJ@163.com>
2020-11-26 18:17:25 +0800

    [ADD] 在python3中, 逐行读取文件

---
+++ com/chapter_10/section_01/task_10.1.2_readFileLine.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/11/26 17:41
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_10.1.2_readFileLine.py
+# @Software: PyCharm
+
+# 逐行读取文件
+# rstrip()： 删除空白行
+
+import time
+
+file_name = 'pi_digits.txt'
+
+time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+with open(file_name) as file_object:
+    for line in file_object:
+        print(time_str + " " + line.rstrip())
