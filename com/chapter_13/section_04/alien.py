b0ab155c15c8c67a9d9dd3c4ede4fdd4b60244ab
StevenGeGe <Yong_GJ@163.com>
2020-12-03 20:48:08 +0800

    [FIX] 在python3中, 外星人入侵游戏，检查外星人是否撞到边缘

--- com/chapter_13/section_04/alien.py
+++ com/chapter_13/section_04/alien.py
@@ -37,5 +37,13 @@ class Alien(Sprite):
 
     def update(self):
         """向右移动外星人, 更新外星人位置"""
-        self.x += self.ai_settings.alien_speed_factor
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
         self.rect.x = self.x
+
+    def check_edges(self):
+        """如果外星人位于屏幕边缘，就返回True"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
