dbfca79d29f9b8b28b2b8050001590323df75b92
StevenGeGe <Yong_GJ@163.com>
2020-12-07 20:37:19 +0800

    [FIX] 在python3中, 外星人入侵游戏， 将消灭的每个外星人的点数都计入得分

--- com/chapter_14/section_01/game_functions.py
+++ com/chapter_14/section_01/game_functions.py
@@ -137,8 +137,9 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
     # 如果是这样，就删除相应的子弹和外星人
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     if collisions:
-        stats.score += ai_settings.alien_points
-        sb.prep_score()
+        for aliens in collisions.values():
+            stats.score += ai_settings.alien_points * len(aliens)
+            sb.prep_score()
     if len(aliens) == 0:
         # 删除现有的子弹，加快游戏节奏，并创建一群新的外星人
         bullets.empty()
