3bcecbef634c0d2baca031e1d55ec04add61af21
StevenGeGe <Yong_GJ@163.com>
2020-12-08 14:47:33 +0800

    [ADD] 在python3中, 数据可视化， scatter绘制单个点

---
+++ com/chapter_15/section_01/task_15-1-3_ scatter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/12/8 14:40
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : task_15-1-3_ scatter.py
+# @Software: PyCharm
+
+# scatter(): 绘制散点图并设置样式
+# 传递一对，x和y坐标，它将在指定位置绘制一个点。
+
+
+import matplotlib.pyplot as plt
+
+plt.scatter(2, 4)
+plt.show()
