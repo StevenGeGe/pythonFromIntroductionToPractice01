ebad438e33966c25bb2bb577cff4c6e85bbb43bf
StevenGeGe <Yong_GJ@163.com>
2020-12-08 16:41:35 +0800

    [ADD] 在python3中, 数据可视化， scatter使用RGB颜色模式指定散点颜色

--- com/chapter_15/section_01/task_15-1-5_ scatter.py
+++ com/chapter_15/section_01/task_15-1-5_ scatter.py
@@ -27,10 +27,16 @@ plt.rcParams['axes.unicode_minus'] = False
 # 自动计算数据
 x_values = list(range(1, 1001))
 y_values = [x ** 2 for x in x_values]
-# edgecolor='none': 删除数据点的轮廓
-plt.scatter(x_values, y_values, edgecolor='none', s=10)
-# plt.scatter(x_values, y_values, s=10)
 
+# s=10: 散点大小
+# plt.scatter(x_values, y_values, s=10)
+# edgecolor='none': 删除数据点的轮廓
+# plt.scatter(x_values, y_values, edgecolor='none', s=10)
+# c='red' : 自定义散点的颜色
+# plt.scatter(x_values, y_values, c='red', edgecolor='none', s=10)
+# c=(0, 0, 0.8)： 使用RGB颜色模式，自定义颜色。
+# 值越接近0，指定的颜色越深，值越接近1，指定的颜色越浅。
+plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
 
 # 设置图表标题
 plt.title("平方数散点图", fontsize=24)
