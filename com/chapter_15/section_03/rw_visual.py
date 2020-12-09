46a35fc462c99978c28227fc7dbc9067200bbce8
StevenGeGe <Yong_GJ@163.com>
2020-12-09 15:57:03 +0800

    [ADD] 在python3中, 数据可视化， 调整屏幕尺寸

--- com/chapter_15/section_03/rw_visual.py
+++ com/chapter_15/section_03/rw_visual.py
@@ -7,16 +7,23 @@
 # @File    : rw_visual.py
 # @Software: PyCharm
 
+
+# 函数figure() 用于指定图表的宽度、高度、分辨率和背景色。
+#   形参figsize 指定一个元组，向matplotlib指出绘图窗口的尺寸，单位为英寸。
+
 import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-plt.figure()
 # 只要程序处于活动状态，就不断地模拟随机漫步
 while True:
     # 创建一个RandomWalk实例，并将其包含的点都绘制出
     rw = RandomWalk(50000)
     rw.fill_walk()
+
+    # 设置绘图窗口的尺寸
+    # dpi: 系统的分辨率
+    plt.figure(dpi=128, figsize=(20, 16))
 
     point_numbers = list(range(rw.num_points))
     # 使用颜色映射来指出漫步中各点的先后顺序，并删除每个点的黑色轮廓，让它们的颜色更明显.
