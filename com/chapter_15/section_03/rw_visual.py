b21daba1f1357827cbf708ed6c42ed3605d55748
StevenGeGe <Yong_GJ@163.com>
2020-12-09 14:59:51 +0800

    [ADD] 在python3中, 数据可视化， 增加点数

--- com/chapter_15/section_03/rw_visual.py
+++ com/chapter_15/section_03/rw_visual.py
@@ -15,13 +15,13 @@ plt.figure()
 # 只要程序处于活动状态，就不断地模拟随机漫步
 while True:
     # 创建一个RandomWalk实例，并将其包含的点都绘制出
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_points))
     # 使用颜色映射来指出漫步中各点的先后顺序，并删除每个点的黑色轮廓，让它们的颜色更明显.
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
-                cmap=plt.cm.Blues, edgecolors='none', s=15)
+                cmap=plt.cm.Blues, edgecolors='none', s=1)
     # 重新绘制起点和终点，并突出起点和终点
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
