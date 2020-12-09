72f53e8be606e6b266b0fd678d191b57a0e3cff6
StevenGeGe <Yong_GJ@163.com>
2020-12-09 16:26:14 +0800

    [ADD] 在python3中, 数据可视化， randint()掷骰子

---
+++ com/chapter_15/section_04/die_visual.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Time    : 2020/12/9 16:21
+# @Author  : Yong
+# @Email   : Yong_GJ@163.com
+# @File    : die_visual.py
+# @Software: PyCharm
+
+from die import Die
+
+# 创建一个D6
+die = Die()
+
+# 掷几次骰子，并将结果存储在一个列表中
+results = []
+for roll_num in range(100):
+    result = die.roll()
+    results.append(result)
+
+print(results)
