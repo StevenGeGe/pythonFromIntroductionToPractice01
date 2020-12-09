e7aa1c1c78657b4491e0c73c787db1a1f7341a94
StevenGeGe <Yong_GJ@163.com>
2020-12-09 19:35:15 +0800

    [ADD] 在python3中, 数据可视化， pygal绘制直方图

--- com/chapter_15/section_04/die_visual.py
+++ com/chapter_15/section_04/die_visual.py
@@ -7,7 +7,10 @@
 # @File    : die_visual.py
 # @Software: PyCharm
 
+# 直方图是一种条形图，指出了各种结果出现的频率。
+
 from die import Die
+import pygal
 
 # 创建一个D6
 die = Die()
@@ -24,4 +27,15 @@ for value in range(1, die.num_sides + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+# 对结果可视化
+hist = pygal.Bar()
+
+hist.title = "滚动一个D6 1000次的结果"
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist._x_title = "结果"
+hist._y_title = "频率结果"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
+# 查看生成的地址， 浏览器访问 ：
+#   file:///F:/data/workspace_data/python_data/pythonFromIntroductionToPractice01/com/chapter_15/section_04/die_visual.svg
